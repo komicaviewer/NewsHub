c591ce91bf4d43629c579171a4364dfd62c2a885
twkevinzhang <twkevinzhang@gmail.com>
2026-04-05 09:48:26 +0800

    fix(boards): opt-in ExperimentalFoundationApi for stickyHeader

--- app/src/main/java/tw/kevinzhang/newshub/ui/boards/BoardsScreen.kt
+++ app/src/main/java/tw/kevinzhang/newshub/ui/boards/BoardsScreen.kt
@@ -10,6 +10,7 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.ExperimentalFoundationApi
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
 import androidx.compose.material3.Checkbox
@@ -39,7 +40,7 @@ import tw.kevinzhang.newshub.R
 import tw.kevinzhang.newshub.auth.LoginStatus
 import tw.kevinzhang.newshub.ui.component.AppCard
 
-@OptIn(ExperimentalMaterial3Api::class)
+@OptIn(ExperimentalMaterial3Api::class, ExperimentalFoundationApi::class)
 @Composable
 fun BoardsScreen(
     onNavigateToMarketplace: () -> Unit,
